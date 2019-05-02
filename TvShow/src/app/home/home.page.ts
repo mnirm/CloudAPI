0e122d2dc6461f7d82ad0aede1ee06bebfef1bd7
jarnomehj <coolsjarno@gmail.com>
2019-05-02 15:55:57 +0200

    Able to receive array from api => fix array as Interface

--- TvShow/src/app/home/home.page.ts
+++ TvShow/src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ShowsService, IShowInfo } from '../services/shows.service';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
+  ShowsLookedUp: IShowInfo;
+  search: string;
+  constructor(private showSvc: ShowsService) {
+    console.log(this.showSvc.getShows());
+  }
+  lookup() {
+    console.log(this.search);
+    this.showSvc.getSpecificShows(this.search).subscribe(success => {
+      this.ShowsLookedUp = success;
+    });
+  }
 }
